fd495cce768ef8bfa9a58ec9c9e2b7ce087a3cbb
Alex Lucas <18901548+AlexandrLucas@users.noreply.github.com>
2021-07-19 01:21:57 +0100

    Unwrap .launch file and clarify demo

--- src/iiwa_move_group_demo.py
+++ src/iiwa_move_group_demo.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
-
+"""
+Move Group functionality demo using a KUKA LBR iiwa simulated model. 
+"""
 # Python 2/3 compatibility imports
 from __future__ import print_function
 from six.moves import input
 
-import argparse
+# Standard Library
 import os
 import sys
 import copy
+
+# ROS and MoveIt!
 import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
+
+# Helper functions
+from moveit_commander.conversions import pose_to_list
 from tf.transformations import quaternion_from_euler
 try:
   from math import pi, tau, dist, fabs, cos
@@ -20,11 +27,48 @@ except: # For Python 2 compatibility
   tau = 2.0*pi
   def dist(p, q):
     return sqrt(sum((p_i - q_i)**2.0 for p_i, q_i in zip(p,q)))
-from std_msgs.msg import String
-from moveit_commander.conversions import pose_to_list
+
+# Set up a ROS namespace to be used in this script.
+##NOTE A more elegant way is to set up a dedicated .launch file and use a 'ns' arg,
+## but this will have to do 
+ns = '/iiwa'
+os.environ["ROS_NAMESPACE"] = ns
+
+# First initialise `moveit_commander`_ and a `rospy`_ node:
+moveit_commander.roscpp_initialize(sys.argv)
+rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
+
+# Instantiate a `RobotCommander`_ object.
+# Provides information such as the robot's kinematic model and the robot's current joint states
+robot = moveit_commander.RobotCommander()
+
+# Instantiate a `PlanningSceneInterface`_ object.
+# This provides a remote interface for getting, setting, and updating the 
+# robot's internal understanding of the surrounding world:
+scene = moveit_commander.PlanningSceneInterface()
+
+# Instantiate a `MoveGroupCommander`_ object.  This object is an interface
+# to a planning group (group of joints).  In this tutorial the group is the primary
+# arm joints in the KUKA LBR iiwa robot, so we set the group's name to "manipulator".
+# This interface can be used to plan and execute motions:
+group_name = "manipulator"
+move_group = moveit_commander.MoveGroupCommander(group_name)
+
+# Relax the goal tolerances a bit when executing a plan
+move_group.set_goal_joint_tolerance(0.001) # default 0.0001
+move_group.set_goal_position_tolerance(0.001) # default 0.0001
+move_group.set_goal_orientation_tolerance(0.01) # default 0.001
+
+# Create a `DisplayTrajectory`_ ROS publisher which is used to display
+# trajectories in Rviz:
+display_trajectory_publisher = rospy.Publisher(
+        ns + '/move_group/display_planned_path',
+        moveit_msgs.msg.DisplayTrajectory,
+        queue_size=20
+)
 
 
-
+target_tolerance = 0.02
 def all_close(goal, actual, tolerance):
   """
   Convenience method for testing if the values in two lists are within a tolerance of each other.
@@ -54,83 +98,44 @@ def all_close(goal, actual, tolerance):
 
   return True
 
-## Set the ROS namespace to be used in this script.
-##NOTE A more elegant way is to set up 'ns' in the .launch file, but this will have to do 
-ns = '/iiwa'
-os.environ["ROS_NAMESPACE"] = ns
-
-## First initialize `moveit_commander`_ and a `rospy`_ node:
-moveit_commander.roscpp_initialize(sys.argv)
-rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
-
-## Instantiate a `RobotCommander`_ object. Provides information such as the robot's kinematic model and the robot's current joint states
-robot = moveit_commander.RobotCommander()
-
-## Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface for getting, setting, and updating the robot's internal understanding of the
-## surrounding world:
-# scene = moveit_commander.PlanningSceneInterface()
-
-## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
-## to a planning group (group of joints).  In this tutorial the group is the primary
-## arm joints in the KUKA iiwa robot, so we set the group's name to "manipulator".
-## This interface can be used to plan and execute motions:
-group_name = "manipulator"
-move_group = moveit_commander.MoveGroupCommander(group_name)
-
-## Create a `DisplayTrajectory`_ ROS publisher which is used to display
-## trajectories in Rviz:
-display_trajectory_publisher = rospy.Publisher(ns + '/move_group/display_planned_path',
-                                                moveit_msgs.msg.DisplayTrajectory,
-                                                queue_size=20)
-
-
-target_tolerance = 0.01
-## Some joint states are named for convenience
-joints_start = [0] * 7  # array of 7 zeroes
-joints_small_shift = [0.3] * 7
-## If iiwa were to get in a position to pick up an object from a table, 
-## it might look something like the following pose 
-pose_ready_to_pick_up = geometry_msgs.msg.Pose()
-q = quaternion_from_euler(3.14, 0, 0)
-pose_ready_to_pick_up.orientation.x = q[0]
-pose_ready_to_pick_up.orientation.y = q[1]
-pose_ready_to_pick_up.orientation.z = q[2]
-pose_ready_to_pick_up.orientation.w = q[3]
-pose_ready_to_pick_up.position.x = 0.4
-pose_ready_to_pick_up.position.y = -0.4
-pose_ready_to_pick_up.position.z = 0.7
-
 
 def go_to_joint_state(joint_goal):
   """
-  The functions takes joint_goal as input, which is is an array of joint positions expressed in radians.
-  In case of KUKA iiwa, there are 7 joints.    
+  Takes joint goal as input, which is is an array (vector) of joint angles expressed in radians.
+  The 'go' command is executed immediately, there is no separate planning stage. 
   """
-  print("Calling 'Go to joint state' with the following joint goal: {}".format(joint_goal))
-  # The go command can be called with joint values, poses, or without any
-  # parameters if you have already set the pose or joint target for the group
-  # wait=True means the call is blocking
+  print("Calling 'Go to joint state' with the following goal: {}".format(joint_goal))
+  # The 'go' command can be called with joint values, poses, or without any
+  # parameters if you have already set the pose or joint target for the group.
+  ##NOTE: wait=True means the call is blocking.
   move_group.go(joint_goal, wait=True)
 
   # Calling ``stop()`` ensures that there is no residual movement
   move_group.stop()
 
-  # Check if arrived at destination
+  # Double check if arrived at destination
   current_joints = move_group.get_current_joint_values()
   if all_close(joint_goal, current_joints, target_tolerance):
-    print("Target state reached")
+    print("Target joint state reached")
   else:
-    print("Target state NOT reached")
+    print("Warning, target joint state NOT reached!")
+    print(current_joints)
 
-def go_to_pose_goal(pose):
-  print("Calling 'Go to pose' with the following goal: \n{}".format(pose))
-  move_group.set_pose_target(pose)
 
-  ## Now, we call the planner to compute the plan and execute it.
+def go_to_pose_goal(pose_goal):
+  """
+  Takes a pose goal as input, and sets it as a target for the planner.
+  After the resulting plan is executed, the target pose is cleared. 
+  """
+  print("Calling 'Go to pose' with the following goal: \n{}".format(pose_goal))
+  # A pose target is set in the world for the end-effector
+  move_group.set_pose_target(pose_goal)
+
+  # Now, we call the planner to compute the plan and execute it.
   plan = move_group.go(wait=True)
 
   # It is always good to clear your targets after planning with poses.
-  # Note: there is no equivalent function for clear_joint_value_targets()
+  ##NOTE: there is no equivalent function for clear_joint_value_targets()
   move_group.clear_pose_targets()
 
   # Calling ``stop()`` ensures that there is no residual movement
@@ -138,30 +143,20 @@ def go_to_pose_goal(pose):
   
   # Check if arrived at destination
   current_pose = move_group.get_current_pose().pose
-  if all_close(pose, current_pose, target_tolerance):
+  if all_close(pose_goal, current_pose, target_tolerance):
     print("Target pose reached")
   else:
-    print("Target pose NOT reached")
+    print("Warning, target pose NOT reached!")
+    print(current_pose)
 
-def plan_cartesian_path(scale=1):
 
-  ## You can plan a Cartesian path directly by specifying a list of waypoints
-  ## for the end-effector to go through. If executing interactively in a
-  ## Python shell, set scale = 1.0.
-  ##
-  waypoints = []
-
-  wpose = move_group.get_current_pose().pose
-  wpose.position.z -= scale * 0.1  # First move up (z)
-  wpose.position.y += scale * 0.2  # and sideways (y)
-  waypoints.append(copy.deepcopy(wpose))
-
-  wpose.position.x += scale * 0.1  # Second move forward/backwards in (x)
-  waypoints.append(copy.deepcopy(wpose))
-
-  wpose.position.y -= scale * 0.1  # Third move sideways (y)
-  waypoints.append(copy.deepcopy(wpose))
-
+def plan_cartesian_path(waypoints):
+  """
+  Compute a sequence of waypoints that make the end-effector move in straight 
+  line segments that follow the poses specified as waypoints. Configurations are 
+  computed for every eef_step meters; The jump_threshold specifies the maximum 
+  distance in configuration space between consecutive points in the resulting path. 
+  """
   # We want the Cartesian path to be interpolated at a resolution of 1 cm
   # which is why we will specify 0.01 as the eef_step in Cartesian
   # translation.  We will disable the jump threshold by setting it to 0.0,
@@ -172,63 +167,112 @@ def plan_cartesian_path(scale=1):
                                       0.01,        # eef_step
                                       0.0)         # jump_threshold
 
-  # Note: We are just planning, not asking move_group to actually move the robot yet:
+  # The return value is a tuple: the actual RobotTrajectory and 
+  # a fraction of how much of the path was followed
+  ##NOTE: We are just planning, not asking move_group to actually move the robot yet
   return plan, fraction
-
-  ## END_SUB_TUTORIAL
 
 
 def display_trajectory(plan):
-  ## BEGIN_SUB_TUTORIAL display_trajectory
-  ##
-  ## Displaying a Trajectory
-  ## ^^^^^^^^^^^^^^^^^^^^^^^
-  ## You can ask RViz to visualize a plan (aka trajectory) for you. But the
-  ## group.plan() method does this automatically so this is not that useful
-  ## here (it just displays the same trajectory again):
-  ##
-  ## A `DisplayTrajectory`_ msg has two primary fields, trajectory_start and trajectory.
-  ## We populate the trajectory_start with our current robot state to copy over
-  ## any AttachedCollisionObjects and add our plan to the trajectory.
+  """
+  RViz can visualize a plan (aka trajectory) for you. But the
+  group.plan() method does this automatically so this is not that useful
+  here (it just displays the same trajectory again):
+  """
+  # A `DisplayTrajectory`_ msg has two primary fields, trajectory_start and trajectory.
+  # We populate the trajectory_start with our current robot state to copy over
+  # any AttachedCollisionObjects and add our plan to the trajectory.
   display_trajectory = moveit_msgs.msg.DisplayTrajectory()
   display_trajectory.trajectory_start = robot.get_current_state()
   display_trajectory.trajectory.append(plan)
   # Publish
   display_trajectory_publisher.publish(display_trajectory)
 
-  ## END_SUB_TUTORIAL
 
 
 def main():
   try:
-    ## Task 0: ensure the arm is at starting position
+    # Define some named joint states for convenience
+    joints_start = [0.0] * 7  # Array of 7 zeroes, coincides with the starting position
+    joints_small_shift = [0.1] * 7 # A slight rotation applied to all joints
+    joints_shift_reverse = [-0.3] * 7 # A slight rotation applied to all joints
+    joints_ascending_shift = list((x/10.0 for x in range(1, 8, 1))) # Why not
+
+
+    ## Task 0
     if not all_close(joints_start, move_group.get_current_joint_values(), target_tolerance):
-      print("Moving to starting position")
+      print('\nTask0: Ensure the arm is at starting position')
+      print("Moving to starting position...")
       go_to_joint_state(joints_start)
 
-    ## Task 1: Actuate all the joints a bit to go to a slightly different position 
-    go_to_joint_state(joints_small_shift)
-    rospy.sleep(1.)
-    print("Moving back to starting position")
-    go_to_joint_state(joints_start)
- 
-    ## Task 2: Go to pose using planner 
+
+    ## Task 1
+    print('\nTask1: Manipulating joint angles directly to move to a different state')
+    input("Press 'Enter' to continue; 'Ctrl+D' to exit\n")
+    move_sequence = [
+      joints_small_shift, 
+      joints_shift_reverse, 
+      joints_ascending_shift
+    ]
+    for i in range(3):
+      go_to_joint_state(move_sequence[i])
+      rospy.sleep(1.)
+    
+
+    ## Task 2
+    print('\nTask 2: Setting a pose in space for the end-effector, computing a trajectory to it and executing the plan') 
+    input("Press 'Enter' to continue; 'Ctrl+D' to exit\n")
+    ## If iiwa were to get in a position to pick up an object from a table, 
+    ## it might look something like the following pose 
+    pose_ready_to_pick_up = geometry_msgs.msg.Pose()
+    q = quaternion_from_euler(pi, 0, 0)
+    pose_ready_to_pick_up.orientation.x = q[0]
+    pose_ready_to_pick_up.orientation.y = q[1]
+    pose_ready_to_pick_up.orientation.z = q[2]
+    pose_ready_to_pick_up.orientation.w = q[3]
+    pose_ready_to_pick_up.position.x = 0.4
+    pose_ready_to_pick_up.position.y = -0.4
+    pose_ready_to_pick_up.position.z = 0.5
+
     go_to_pose_goal(pose_ready_to_pick_up)
-    rospy.sleep(1.)
+    
 
-    ## Task 3: Plan a trajectory in Cartesian coordinates and execute
-    # Using the previous pose as a starting position,
-    # do some movements that might look like we are trying to move something on the table  
-    cartesian_plan, fraction = plan_cartesian_path()
+    ## Task 3
+    print('\nTask 3: Plan a trajectory in Cartesian coordinates from a list of waypoints and execute') 
+    input("Press 'Enter' to continue; 'Ctrl+D' to exit\n")
+    # Using the previous pose as a starting position, specify a few waypoints
+    # for the end-effector to go through. The waypoints are selected such that 
+    # overall it resembles a Pick-and-Place operation.  
+
+    # Set up some waypoints
+    waypoints = []
+    scale = 1.0
+    wpose = move_group.get_current_pose().pose
+    # Waypoint 1
+    wpose.position.z -= scale * 0.1  # First move up (z)
+    wpose.position.y += scale * 0.2  # and sideways (y)
+    waypoints.append(copy.deepcopy(wpose))
+    # Waypoint 2
+    wpose.position.x += scale * 0.1  # Second move forward/backwards in (x)
+    wpose.orientation.x += 1  # Also change orientation
+    waypoints.append(copy.deepcopy(wpose))
+    # Waypoint 3
+    wpose.position.y -= scale * 0.1  # Third move sideways (y)
+    wpose.position.z += scale * 0.1  # and down (z)
+    wpose.orientation.x -= 1  # # Restore orientation
+    waypoints.append(copy.deepcopy(wpose))
+
+    # Plan trajectory
+    cartesian_plan, fraction = plan_cartesian_path(waypoints)
     display_trajectory(cartesian_plan)
+
+    # Execute
     move_group.execute(cartesian_plan, wait=True)
-    print("Moving back to starting position")
+
+    print("Moving back to starting position...")
     go_to_joint_state(joints_start)
 
-
-  except rospy.ROSInterruptException:
-    return
-  except KeyboardInterrupt:
+  except (rospy.ROSInterruptException, EOFError, KeyboardInterrupt) as e:
     return
 
 if __name__ == '__main__':
